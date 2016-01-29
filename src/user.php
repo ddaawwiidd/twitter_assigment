7eb3b09c4f98065808be1a760f679c9020114fdb
dd <jankowskidawid@gmail.com>
2016-01-29 15:47:41 +0100

    Finished (almost) User class

--- src/user.php
+++ src/user.php
@@ -1,6 +1,108 @@
 <?php
+
+/*
+ * CREATE TABLE Users(
+ * id int AUTO_INCREMENT,
+ * name varchar(255),
+ * email varchar(255) UNIQUE,
+ * password char(60),
+ * description varchar(255),
+ * PRIMARY KEY(id)
+ * );
+ */
 class User
 {
+    static private $connection;
+
+    static public function SetConnection(mysqli $newConnection)
+    {
+        User::$connection = $newConnection;
+    }
+
+
+    static public function RegisterUser($newName, $newEmail, $password1, $password2, $newDescription)
+    {
+        if($password1 != $password2)
+        {
+            return false;
+        }
+        $options = [
+        'cost' => 11,
+        'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
+        ];
+        $hashedPassword = password_hash($password1, PASSWORD_BCRYPT, $options);
+
+        $sql = "INSERT INTO Users (name, email, password, description)
+        VALUES ('$newName', '$newEmail', '$hashedPassword', '$newDescription')";
+
+
+        $result = self::$connection->query($sql);
+        if($result === true)
+        {
+            $newUser = new User(self::$connection->insert_id, $newName, $newEmail, $newDescription);
+            return $newUser;
+        }
+        return false;
+    }
+
+    static public function LogInUser ($email, $password)
+    {
+        $sql = "SELECT * FROM Users WHERE email LIKE '$email'";
+        $result = self::$connection->query($sql);
+
+        if($result != false)
+        {
+            if($result->num_rows == 1)
+            {
+                $row = $result->fetch_assoc();
+                $isPasswordOk = password_verify($password, $row["password"]);
+                if($isPasswordOk === true)
+                {
+                    $user = new User($row["id"], $row["name"], $row["email"], $row["description"]);
+                    return $user;
+                }
+            }
+        }
+        return false;
+    }
+
+    static public function GetUserById ($idToLoad)
+    {
+        $sql = "SELECT * FROM Users WHERE id = $idToLoad";
+        $result = self::$connection->query($sql);
+
+        if($result != false)
+        {
+            if($result->num_rows == 1)
+            {
+                $row = $result->fetch_assoc();
+                $user = new User($row["id"], $row["name"], $row["email"], $row["description"]);
+                return $user;
+            }
+        }
+        return false;
+    }
+
+    static public function GetAllUsers ()
+    {
+        $ret = [];
+        $sql = "SELECT * FROM Users";
+        $result = self::$connection->query($sql);
+
+        if($result != false)
+        {
+            if($result->num_rows > 0)
+            {
+                while($row = $result->fetch_assoc())
+                {
+                    $user = new User($row["id"], $row["name"], $row["email"], $row["description"]);
+                    $ret[] = $user;
+                }
+            }
+        }
+        return $ret;
+    }
+
     private $id;
     private $name;
     private $email;
@@ -41,5 +143,49 @@ class User
             $this->description = $newDescription;
         }
 
+    }
+
+    public function saveToDB ()
+    {
+        $sql = "UPDATE Users SET description='$this->description' WHERE id = $this->id";
+        $result = self::$connection->query($sql);
+        if($result === true)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public function loadAllTweets()
+    {
+        $ret = [];
+
+        //TODO:finish this function
+        //TODO:It should return table of Tweets created by this user (date DESC)
+
+
+        return $ret;
+    }
+
+    public function loadAllSentMessages()
+    {
+        $ret = [];
+
+        //TODO:finish this function
+        //TODO:It should return table of messages send by this user (date DESC)
+
+
+        return $ret;
+    }
+
+    public function loadAllReceivedMessages()
+    {
+        $ret = [];
+
+        //TODO:finish this function
+        //TODO:It should return table of messages received by this user (date DESC)
+
+
+        return $ret;
     }
 }
